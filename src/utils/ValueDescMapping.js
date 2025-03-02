5646952c5ea702910bc1ac4329578b71ad5f7365
AsifZaman777 <eramasif16@gmail.com>
2025-03-02 15:12:50 +0600

    updated vdm

--- src/utils/ValueDescMapping.js
+++ src/utils/ValueDescMapping.js
@@ -1,6 +1,6 @@
 export const valueDescMapping = {
 
-//#region message type 35
+//#region 35
 35:{
     0: "Heartbeat",
     A: "Logon",
@@ -18,8 +18,8 @@ export const valueDescMapping = {
     9:"Order Cancel Reject",
     AR:"Trade Capture Report Ack",
 },
-//#endregion
 
+//#region 59
   59: {
     0: "Day",
     1: "Good till cancelled",
@@ -28,14 +28,45 @@ export const valueDescMapping = {
     4: "Fill or Kill (FoK)",
     6: "Good till date (GTD)",
   },
+
+  //#region 54
   54: {
     1: "Buy",
     2: "Sell",
     3: "Short sell"
   },
+
+  //#region 40
   40: {
     1: "Market",
     2: "Limit",
-  }
-  // ...add more mappings as needed...
+  },
+
+  //#region 1128
+  1128: {
+    8:"FIX50SP1"
+  },
+
+  //#region 447
+  447:{
+    C:"Participant Identifier",
+  },
+
+  //#region 452
+  452:{
+   1:"Executing Firm",
+   3:"Client ID",
+   4:"Clearing Firm",
+   5:"Investor ID (Deposit account)",
+   7:"Entering Firm",
+   11:"Order origination trader",
+   12:"Executing trader",
+   13:"Order origination firm",
+   17:"Contra Firm",
+   36:"Entering trader",
+   37:"Contra trader",
+   44:"Order Entry Operator ID",
+  },
+
+ 
 };
